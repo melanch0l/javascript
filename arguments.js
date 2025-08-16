6599a38da72af51be81441eedd2503f2f433bb42
melanch0l <minnyinyiworkmail@gmail.com>
2025-08-16 23:11:17 +0630

    advanced js concepts

--- arguments.js
+++ arguments.js
@@ -5,7 +5,7 @@ function marry(person1, person2) {
 marry('john', 'ann');
 //using rest parameter(...args)
 function marry2(...args) {
-    console.log("this is execution context argumentes", args);//it looks like array but it's not array
+    console.log("this is execution context argumentes", args);// array 
     console.log(`${args[0]} is married to ${args[1]}`);
 }
 marry2('john', 'ann');
