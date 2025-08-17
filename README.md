6cf07b118930adaa3f0ab2444962cd4fcf8b4d34
melanch0l <minnyinyiworkmail@gmail.com>
2025-08-17 10:49:12 +0630

    test hoisting

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # javascript
+Just Learning Basic JS
--- hositing.js
+++ hositing.js
@@ -1,4 +1,6 @@
 var favFruit = 'mango';
+var fruit = 'apple';
+var fruit = 'orange';
 
 function hisFavFruit() {
     // this this how hoisted looks : var favFruit = undefined;
@@ -9,5 +11,5 @@ function hisFavFruit() {
     console.log("his fav fruit for now is " + favFruit);
 
 }
-
+console.log(fruit);
 hisFavFruit();
